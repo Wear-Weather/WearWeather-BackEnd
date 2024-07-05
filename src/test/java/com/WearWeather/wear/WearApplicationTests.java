399fa094686782ae25d660c722aaf056e6928004
sky1231234 <blueskyee52@gmail.com>
2024-07-05 15:23:29 +0900

    init

---
+++ src/test/java/com/WearWeather/wear/WearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WearWeather.wear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
