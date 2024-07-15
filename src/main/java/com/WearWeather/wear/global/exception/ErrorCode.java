2d1677d57efd5229cec7367e45ab5e4b96adb202
이건희 <dnzp74@g.eulji.ac.kr>
2024-07-15 18:31:04 +0900

    [#4] feat : Redis, Token 관련 에러 코드 추가

--- src/main/java/com/WearWeather/wear/global/exception/ErrorCode.java
+++ src/main/java/com/WearWeather/wear/global/exception/ErrorCode.java
@@ -1,8 +1,10 @@
 package com.WearWeather.wear.global.exception;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import lombok.Getter;
+import org.hibernate.annotations.NotFound;
 import org.springframework.http.HttpStatus;
 
 @Getter
@@ -24,7 +26,11 @@ public enum ErrorCode {
     NOT_EXIST_USER(BAD_REQUEST, "일치하는 계정이 없습니다."),
     NO_SUCH_ALGORITHM(BAD_REQUEST, "해당 알고리즘이 존재하지 않습니다."),
 
-    UNABLE_TO_SEND_EMAIL(BAD_REQUEST,"해당 이메일로 코드를 발송하지 못했습니다.")
+    UNABLE_TO_SEND_EMAIL(BAD_REQUEST,"해당 이메일로 코드를 발송하지 못했습니다."),
+
+    INVALID_REFRESH_TOKEN(BAD_REQUEST,"리프레시 토큰 값이 일치하지 않습니다."),
+
+    REDIS_VALUE_NOT_FOUND(NOT_FOUND,"Redis에 저장된 값을 찾을 수 없습니다.")
     ;
 
     private final HttpStatus httpStatus;
