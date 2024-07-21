23f6225fa8c6b191477baf17ec010e7971807e85
이건희 <dnzp74@g.eulji.ac.kr>
2024-07-21 15:27:43 +0900

    [#6] refactor : Jwt 내에 Spring Security 설정 변경

--- src/main/java/com/WearWeather/wear/global/jwt/CustomUserDetailsService.java
+++ src/main/java/com/WearWeather/wear/global/jwt/CustomUserDetailsService.java
@@ -2,10 +2,6 @@ package com.WearWeather.wear.global.jwt;
 
 import com.WearWeather.wear.user.entity.User;
 import com.WearWeather.wear.user.repository.UserRepository;
-import java.util.List;
-import java.util.stream.Collectors;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -14,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Component("userDetailsService")
 public class CustomUserDetailsService implements UserDetailsService {
+
     private final UserRepository userRepository;
 
     public CustomUserDetailsService(UserRepository userRepository) {
@@ -23,21 +20,10 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     @Transactional
     public UserDetails loadUserByUsername(final String userEmail) {
-        return userRepository.findOneWithAuthoritiesByEmail(userEmail)
-            .map(user -> createUser(userEmail, user))
+        User user = userRepository.findOneWithAuthoritiesByEmail(userEmail)
             .orElseThrow(() -> new UsernameNotFoundException(userEmail + " -> 데이터베이스에서 찾을 수 없습니다."));
+
+        return new CustomUserDetail(user);
     }
 
-    private org.springframework.security.core.userdetails.User createUser(String userEmail, User user) {
-
-        // 유저의 권한 정보들
-        List<GrantedAuthority> grantedAuthorities = user.getAuthorities().stream()
-            .map(authority -> new SimpleGrantedAuthority(authority.getAuthorityName()))
-            .collect(Collectors.toList());
-
-        // 유저 네임, 패스워드, 권한정보를 User 객체로 리턴
-        return new org.springframework.security.core.userdetails.User(user.getEmail(),
-            user.getPassword(),
-            grantedAuthorities);
-    }
 }
