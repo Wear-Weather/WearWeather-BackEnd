1823bc8af1b2c060e556d1b373d2e968ab91a10f
sky1231234 <blueskyee52@gmail.com>
2024-07-27 14:38:39 +0900

    fix: final 변수 추가로 인한 역직렬화 오류 수정

--- src/main/java/com/WearWeather/wear/domain/user/dto/request/ModifyUserInfoRequest.java
+++ src/main/java/com/WearWeather/wear/domain/user/dto/request/ModifyUserInfoRequest.java
@@ -6,19 +6,18 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
-public class FindUserEmailRequest {
+public class ModifyUserInfoRequest {
 
-    @NotBlank(message = "이름은 필수입니다.")
-    private final String name;
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    private final String password;
 
     @NotBlank(message = "닉네임은 필수입니다.")
     private final String nickname;
-
     @JsonCreator
-    public FindUserEmailRequest(
-            @JsonProperty("name") String name,
+    public ModifyUserInfoRequest(
+            @JsonProperty("password") String password,
             @JsonProperty("nickname") String nickname) {
-        this.name = name;
+        this.password = password;
         this.nickname = nickname;
     }
 }
