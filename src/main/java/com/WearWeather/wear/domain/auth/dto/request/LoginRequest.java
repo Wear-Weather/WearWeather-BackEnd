8c8c67fc0b2d69d9f0593386d917e38be61d8b50
sky1231234 <blueskyee52@gmail.com>
2024-07-29 20:21:27 +0900

    fix: final 필드의 역직렬화 오류 수정

--- src/main/java/com/WearWeather/wear/domain/auth/dto/request/LoginRequest.java
+++ src/main/java/com/WearWeather/wear/domain/auth/dto/request/LoginRequest.java
@@ -1,15 +1,17 @@
 package com.WearWeather.wear.domain.auth.dto.request;
 
 import com.WearWeather.wear.domain.user.entity.User;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
-@AllArgsConstructor
 public class LoginRequest {
 
     @NotBlank
@@ -19,6 +21,14 @@ public class LoginRequest {
     @NotBlank
     @Size(min = 3, max = 100)
     private final String password;
+
+    @JsonCreator
+    public LoginRequest(
+            @JsonProperty("email") String email,
+            @JsonProperty("password") String password) {
+        this.email = email;
+        this.password = password;
+    }
 
     public User toEntity() {
         return User.builder()
