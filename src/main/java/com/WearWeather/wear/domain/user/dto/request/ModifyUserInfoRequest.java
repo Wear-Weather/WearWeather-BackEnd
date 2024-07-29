679de904ec633daf928201326e940de35252aa6c
sky1231234 <blueskyee52@gmail.com>
2024-07-29 16:38:23 +0900

    feat: 회원정보 수정 request 추가

---
+++ src/main/java/com/WearWeather/wear/domain/user/dto/request/ModifyUserInfoRequest.java
@@ -0,0 +1,24 @@
+package com.WearWeather.wear.domain.user.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class ModifyUserInfoRequest {
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    private final String password;
+
+    @NotBlank(message = "닉네임은 필수입니다.")
+    private final String nickname;
+
+    @JsonCreator
+    public ModifyUserInfoRequest(
+            @JsonProperty("password") String password,
+            @JsonProperty("nickname") String nickname) {
+        this.password = password;
+        this.nickname = nickname;
+    }
+}
