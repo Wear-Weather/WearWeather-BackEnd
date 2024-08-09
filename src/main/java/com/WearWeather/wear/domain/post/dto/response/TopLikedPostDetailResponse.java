fd3262bd3ef4c6c3269131b0f78ba6d5044059d9
sky1231234 <blueskyee52@gmail.com>
2024-08-09 17:34:53 +0900

    refactor: 태그 필드명 수정

--- src/main/java/com/WearWeather/wear/domain/post/dto/response/TopLikedPostDetailResponse.java
+++ src/main/java/com/WearWeather/wear/domain/post/dto/response/TopLikedPostDetailResponse.java
@@ -15,9 +15,9 @@ public class TopLikedPostDetailResponse {
     private final Long postId;
     private final String thumbnail;
     private final Location location;
-    private final Long seasonTag;
-    private final List<Long> weatherTags;
-    private final List<Long> temperatureTags;
+    private final Long seasonTagId;
+    private final List<Long> weatherTagIds;
+    private final List<Long> temperatureTagIds;
     private final boolean likeByUser;
 
     public static TopLikedPostDetailResponse of(Post post, String url, Long seasonTag, List<Long> weatherTags, List<Long> temperatureTags, boolean like){
@@ -25,9 +25,9 @@ public class TopLikedPostDetailResponse {
                 .postId(post.getPostId())
                 .thumbnail(url) //TODO : URL 만드는 법 확인
                 .location(post.getLocation())
-                .seasonTag(seasonTag)
-                .weatherTags(weatherTags)
-                .temperatureTags(temperatureTags)
+                .seasonTagId(seasonTag)
+                .weatherTagIds(weatherTags)
+                .temperatureTagIds(temperatureTags)
                 .likeByUser(like)
                 .build();
     }
