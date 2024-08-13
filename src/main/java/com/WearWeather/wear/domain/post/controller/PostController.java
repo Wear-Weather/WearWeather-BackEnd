1bfaddc086588d6033a4be8bbb37136200e51877
sky1231234 <blueskyee52@gmail.com>
2024-08-13 17:35:29 +0900

    refactor: @Valid 어노테이션 추가

--- src/main/java/com/WearWeather/wear/domain/post/controller/PostController.java
+++ src/main/java/com/WearWeather/wear/domain/post/controller/PostController.java
@@ -9,6 +9,7 @@ import com.WearWeather.wear.domain.post.dto.response.TopLikedPostsResponse;
 import com.WearWeather.wear.domain.post.service.PostService;
 import com.WearWeather.wear.global.common.ResponseMessage;
 import com.WearWeather.wear.global.common.dto.ResponseCommonDTO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -25,7 +26,7 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping
-    public ResponseEntity<ResponseCommonDTO> createPost(@AuthenticationPrincipal UserDetails userDetails, @RequestBody PostCreateRequest request) {
+    public ResponseEntity<ResponseCommonDTO> createPost(@AuthenticationPrincipal UserDetails userDetails, @Valid @RequestBody PostCreateRequest request) {
         Long postId = postService.createPost(userDetails.getUsername(), request);
         return ResponseEntity.ok(new ResponseCommonDTO(true, ResponseMessage.SUCCESS_POST));
     }
@@ -42,7 +43,7 @@ public class PostController {
     }
 
     @GetMapping
-    public ResponseEntity<PostsByLocationResponse> getPostsByLocation(@AuthenticationPrincipal UserDetails userDetails, @RequestBody PostsByLocationRequest request) {
+    public ResponseEntity<PostsByLocationResponse> getPostsByLocation(@AuthenticationPrincipal UserDetails userDetails, @Valid @RequestBody PostsByLocationRequest request) {
         return ResponseEntity.ok(postService.getPostsByLocation(userDetails.getUsername(), request));
     }
 }
