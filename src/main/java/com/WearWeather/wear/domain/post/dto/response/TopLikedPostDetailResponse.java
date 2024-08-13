74df9530a7c0e22d457f3e1588b33f6a57ffdaaa
이건희 <dnzp74@g.eulji.ac.kr>
2024-08-14 08:01:41 +0900

    refactor : Post pk id 필드명 postId -> id로 수정
    - Id 네임으로도 pk가 id임을 명확히 알 수 있다.
    - 중복되는 코드를 줄이고자 함 ex) post.getPostId();

--- src/main/java/com/WearWeather/wear/domain/post/dto/response/TopLikedPostDetailResponse.java
+++ src/main/java/com/WearWeather/wear/domain/post/dto/response/TopLikedPostDetailResponse.java
@@ -1,12 +1,12 @@
 package com.WearWeather.wear.domain.post.dto.response;
+
 import com.WearWeather.wear.domain.post.entity.Location;
 import com.WearWeather.wear.domain.post.entity.Post;
+import java.util.List;
+import java.util.Map;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-
-import java.util.List;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @Getter
@@ -21,16 +21,16 @@ public class TopLikedPostDetailResponse {
     private final List<Long> temperatureTagIds;
     private final boolean likeByUser;
 
-    public static TopLikedPostDetailResponse of(Post post, String url, Map<String, List<Long>> tags, boolean like){
+    public static TopLikedPostDetailResponse of(Post post, String url, Map<String, List<Long>> tags, boolean like) {
         return TopLikedPostDetailResponse.builder()
-                .postId(post.getPostId())
-                .thumbnail(url)
-                .location(post.getLocation())
-                .seasonTagId(tags.get("SEASON").get(0))
-                .weatherTagIds(tags.get("WEATHER"))
-                .temperatureTagIds(tags.get("TEMPERATURE"))
-                .likeByUser(like)
-                .build();
+            .postId(post.getId())
+            .thumbnail(url)
+            .location(post.getLocation())
+            .seasonTagId(tags.get("SEASON").get(0))
+            .weatherTagIds(tags.get("WEATHER"))
+            .temperatureTagIds(tags.get("TEMPERATURE"))
+            .likeByUser(like)
+            .build();
     }
 
 }
