5729138075143fbb321ed99340093d77819939ef
sky1231234 <blueskyee52@gmail.com>
2024-08-16 06:50:14 +0900

    feat: 서브모듈 최신화 - RDS 설정 파일 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,26 +1,26 @@
-
-insert into "user" (email, password, name, nickname, is_social) values ('admin@naver.com', '$2a$12$4EbNBPJzszu/DQ11QEgdTuBoxPjZFV0EhOPWusw7FzSYt3.WMjMvK', 'admin', 'admin', false);
-insert into "user" (email, password, name, nickname, is_social) values ('geonHui@naver.com', '$2a$12$kwmjJetpHfDH9IV2843.suBCzpiL.AIyfdKCAG/syMsP.P3KFx/2m', 'geonHui', 'geonHui', false);
-
-insert into authority (authority_name) values ('ROLE_USER');
-insert into authority (authority_name) values ('ROLE_ADMIN');
-
-insert into user_authority (user_id, authority_name) values (1, 'ROLE_USER');
-insert into user_authority (user_id, authority_name) values (1, 'ROLE_ADMIN');
-insert into user_authority (user_id, authority_name) values (2, 'ROLE_USER');
-
-
-INSERT INTO tag (category, content) VALUES ('WEATHER', 'SUNNY');
-INSERT INTO tag (category, content) VALUES ('WEATHER', 'CLOUDY');
-INSERT INTO tag (category, content) VALUES ('WEATHER', 'RAINY');
-INSERT INTO tag (category, content) VALUES ('WEATHER', 'SNOWY');
-INSERT INTO tag (category, content) VALUES ('WEATHER', 'WINDY');
-INSERT INTO tag (category, content) VALUES ('TEMPERATURE', 'HOT');
-INSERT INTO tag (category, content) VALUES ('TEMPERATURE', 'COLD');
-INSERT INTO tag (category, content) VALUES ('TEMPERATURE', 'WARM');
-INSERT INTO tag (category, content) VALUES ('TEMPERATURE', 'COOL');
-INSERT INTO tag (category, content) VALUES ('TEMPERATURE', 'MILD');
-INSERT INTO tag (category, content) VALUES ('SEASON', 'SPRING');
-INSERT INTO tag (category, content) VALUES ('SEASON', 'SUMMER');
-INSERT INTO tag (category, content) VALUES ('SEASON', 'FALL');
-INSERT INTO tag (category, content) VALUES ('SEASON', 'WINTER');
+--
+--insert into "user" (email, password, name, nickname, is_social) values ('admin@naver.com', '$2a$12$4EbNBPJzszu/DQ11QEgdTuBoxPjZFV0EhOPWusw7FzSYt3.WMjMvK', 'admin', 'admin', false);
+--insert into "user" (email, password, name, nickname, is_social) values ('geonHui@naver.com', '$2a$12$kwmjJetpHfDH9IV2843.suBCzpiL.AIyfdKCAG/syMsP.P3KFx/2m', 'geonHui', 'geonHui', false);
+--
+--insert into authority (authority_name) values ('ROLE_USER');
+--insert into authority (authority_name) values ('ROLE_ADMIN');
+--
+--insert into user_authority (user_id, authority_name) values (1, 'ROLE_USER');
+--insert into user_authority (user_id, authority_name) values (1, 'ROLE_ADMIN');
+--insert into user_authority (user_id, authority_name) values (2, 'ROLE_USER');
+--
+--
+--INSERT INTO tag (category, content) VALUES ('WEATHER', 'CLOUDY');
+--INSERT INTO tag (category, content) VALUES ('WEATHER', 'SUNNY');
+--INSERT INTO tag (category, content) VALUES ('WEATHER', 'SNOWY');
+--INSERT INTO tag (category, content) VALUES ('WEATHER', 'RAINY');
+--INSERT INTO tag (category, content) VALUES ('WEATHER', 'WINDY');
+--INSERT INTO tag (category, content) VALUES ('TEMPERATURE', 'HOT');
+--INSERT INTO tag (category, content) VALUES ('TEMPERATURE', 'COLD');
+--INSERT INTO tag (category, content) VALUES ('TEMPERATURE', 'WARM');
+--INSERT INTO tag (category, content) VALUES ('TEMPERATURE', 'COOL');
+--INSERT INTO tag (category, content) VALUES ('TEMPERATURE', 'MILD');
+--INSERT INTO tag (category, content) VALUES ('SEASON', 'SPRING');
+--INSERT INTO tag (category, content) VALUES ('SEASON', 'SUMMER');
+--INSERT INTO tag (category, content) VALUES ('SEASON', 'FALL');
+--INSERT INTO tag (category, content) VALUES ('SEASON', 'WINTER');
