193ff5f8f2841d4a67421082ca34917fb5f245ca
sky1231234 <blueskyee52@gmail.com>
2024-08-20 15:09:27 +0900

    feat: 지역 데이터 전달 api 추가

--- src/main/java/com/WearWeather/wear/domain/location/service/LocationService.java
+++ src/main/java/com/WearWeather/wear/domain/location/service/LocationService.java
@@ -1,5 +1,8 @@
 package com.WearWeather.wear.domain.location.service;
 
+import com.WearWeather.wear.domain.location.dto.response.DistrictResponse;
+import com.WearWeather.wear.domain.location.dto.response.RegionResponse;
+import com.WearWeather.wear.domain.location.dto.response.RegionsResponse;
 import com.WearWeather.wear.domain.location.entity.City;
 import com.WearWeather.wear.domain.location.entity.District;
 import com.WearWeather.wear.domain.location.repository.CityRepository;
@@ -9,6 +12,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.*;
@@ -16,6 +20,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class LocationService {
 
     @Value("${location.api.key}")
@@ -68,5 +73,62 @@ public class LocationService {
                 districtRepository.saveAll(districtsToSave);
             }
         }
+    }
+
+    public RegionsResponse getRegions(){
+        List<City> cityList = cityRepository.findAll();
+        List<District> districts = districtRepository.findAll();
+        Map<Long, List<District>> districtMap = districts.stream()
+                .collect(Collectors.groupingBy(District::getCityId));
+
+        List<RegionResponse> regionResponse = getRegionResponseList(cityList, districtMap);
+
+        return RegionsResponse.of(regionResponse);
+    }
+
+    private List<RegionResponse> getRegionResponseList(List<City> cityList, Map<Long, List<District>> districtMap){
+
+        List<RegionResponse> regionResponse = new ArrayList<>();
+        regionResponse.add(setRegionInitialData());
+        regionResponse.addAll(getRegionResponseListByCity(cityList, districtMap));
+
+        return regionResponse;
+    }
+
+    private List<RegionResponse> getRegionResponseListByCity(List<City> cityList, Map<Long, List<District>> districtMap){
+        return cityList.stream()
+                .filter(city -> districtMap.containsKey(city.getId()))
+                .map(city -> getRegionResponseByCity(city, districtMap.get(city.getId())))
+                .toList();
+    }
+    private RegionResponse getRegionResponseByCity(City city, List<District> districts){
+
+        List<DistrictResponse> districtByCity = new ArrayList<>();
+        districtByCity.add(setDistrictInitialData());
+
+        districtByCity.addAll(getDistrictResponse(districts));
+
+        return RegionResponse.of(city.getId(), city.getCity(), districtByCity);
+    }
+
+    private List<DistrictResponse> getDistrictResponse(List<District> districts) {
+        return districts.stream()
+                .map(district -> DistrictResponse.of(district.getId(), district.getDistrict()))
+                .toList();
+    }
+
+    private RegionResponse setRegionInitialData(){
+        Long cityId = 1L;
+        String cityName = "전체";
+        List<DistrictResponse> emptyResponse = Collections.emptyList();
+
+        return RegionResponse.of(cityId, cityName, emptyResponse);
+    }
+
+    private DistrictResponse setDistrictInitialData(){
+        Long districtId = 0L;
+        String districtName = "전체";
+
+        return DistrictResponse.of(districtId, districtName);
     }
 }
