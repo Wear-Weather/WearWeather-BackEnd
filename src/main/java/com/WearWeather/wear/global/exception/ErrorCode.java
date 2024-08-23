4c52854956670f5e2cb1c61eefc9a4e019bb0e83
이건희 <dnzp74@g.eulji.ac.kr>
2024-08-23 20:20:40 +0900

    bug : 브랜치 머지 후에 생기는 에러로 인한 코드 수정

--- src/main/java/com/WearWeather/wear/global/exception/ErrorCode.java
+++ src/main/java/com/WearWeather/wear/global/exception/ErrorCode.java
@@ -47,7 +47,6 @@ public enum ErrorCode {
     NOT_EXIST_DISTRICT_ID(BAD_REQUEST, "일치하는 시군구가 없습니다."),
     NOT_EXIST_CITY(BAD_REQUEST, "일치하는 광역시도가 없습니다."),
     NOT_EXIST_DISTRICT(BAD_REQUEST, "일치하는 시군구가 없습니다."),
-    ;
 
     HIDDEN_POST_ALREADY_EXIST(BAD_REQUEST, "이미 숨겨진 게시물입니다."),
 
