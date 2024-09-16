a02b288885ca3484c31d6fab0d4033f1309ad0c0
이건희 <dnzp74@g.eulji.ac.kr>
2024-09-16 18:05:03 +0900

    refactor : 충돌 발생하는 예외 처리 값 추가

--- src/main/java/com/WearWeather/wear/global/exception/ErrorCode.java
+++ src/main/java/com/WearWeather/wear/global/exception/ErrorCode.java
@@ -58,6 +58,13 @@ public enum ErrorCode {
 
     KAKAO_USER_NOT_FOUND(BAD_REQUEST, "카카오 사용자 정보가 존재하지 않습니다."),
     INVALID_DELETE_REASON(BAD_REQUEST,"올바르지 않은 탈퇴 이유 값 입니다"),
+
+    UNAUTHORIZED_USER(UNAUTHORIZED,"게시글 수정 권한이 없습니다."),
+    IMAGE_ALREADY_USED(BAD_REQUEST, "이미 다른 게시글에 사용된 이미지ID가 있습니다."),
+
+    FAIL_SAVE_LOCATION_CITY(BAD_REQUEST, "광역시도 저장 오류입니다."),
+    FAIL_SAVE_LOCATION_DISTRICT(BAD_REQUEST, "시군구 저장 오류입니다."),
+    FAIL_CREATE_LOCATION_ACCESS_TOKEN(BAD_REQUEST, "위치 액세스 토큰 생성 오류입니다.."),
     ;
 
     private final HttpStatus httpStatus;
