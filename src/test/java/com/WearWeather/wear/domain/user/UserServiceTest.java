541b7a8edcbf889e9d19b53dda839bd139674b72
이건희 <dnzp74@g.eulji.ac.kr>
2024-09-16 17:56:47 +0900

    test : 회원 탈퇴 기능 단위 테스트 작성

--- src/test/java/com/WearWeather/wear/domain/user/UserServiceTest.java
+++ src/test/java/com/WearWeather/wear/domain/user/UserServiceTest.java
@@ -13,11 +13,18 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.WearWeather.wear.domain.oauth.infrastructure.kakao.dto.KakaoUserDto;
+import com.WearWeather.wear.domain.oauth.infrastructure.kakao.entity.KakaoUser;
+import com.WearWeather.wear.domain.oauth.infrastructure.kakao.service.KakaoUserService;
+import com.WearWeather.wear.domain.oauth.service.RequestOAuthUnlinkService;
+import com.WearWeather.wear.domain.user.dto.request.DeleteReasonRequest;
 import com.WearWeather.wear.domain.user.dto.request.ModifyUserPasswordRequest;
 import com.WearWeather.wear.domain.user.dto.request.RegisterUserRequest;
 import com.WearWeather.wear.domain.user.dto.response.UserInfoResponse;
 import com.WearWeather.wear.domain.user.entity.User;
+import com.WearWeather.wear.domain.user.enums.DeleteReason;
 import com.WearWeather.wear.domain.user.repository.UserRepository;
+import com.WearWeather.wear.domain.user.service.UserDeleteService;
 import com.WearWeather.wear.domain.user.service.UserService;
 import com.WearWeather.wear.fixture.UserFixture;
 import com.WearWeather.wear.global.exception.CustomException;
@@ -37,6 +44,21 @@ public class UserServiceTest {
 
     @InjectMocks
     UserService userService;
+
+    @Mock
+    UserDeleteService userDeleteService;
+
+    @Mock
+    KakaoUserService kakaoUserService;
+
+    @Mock
+    RequestOAuthUnlinkService requestOAuthUnlinkService;
+
+    @Mock
+    KakaoUser kakaoUser;
+
+    @Mock
+    KakaoUserDto kakaoUserDto;
 
     @Mock
     private PasswordEncoder passwordEncoder;
@@ -207,7 +229,7 @@ public class UserServiceTest {
 
         CustomException exception = assertThrows(CustomException.class, () ->
             userService.getUserInfo(UserFixture.userId));
-        assertEquals(ErrorCode.NOT_EXIST_EMAIL, exception.getErrorCode());
+        assertEquals(ErrorCode.NOT_EXIST_USER, exception.getErrorCode());
     }
 
     @Test
@@ -233,7 +255,7 @@ public class UserServiceTest {
 
         CustomException exception = assertThrows(CustomException.class, () ->
             userService.modifyUserInfo(UserFixture.userId, UserFixture.password, UserFixture.nickname));
-        assertEquals(ErrorCode.NOT_EXIST_EMAIL, exception.getErrorCode());
+        assertEquals(ErrorCode.NOT_EXIST_USER, exception.getErrorCode());
     }
 
     @Test
@@ -253,5 +275,100 @@ public class UserServiceTest {
 
         assertEquals(ErrorCode.FAIL_UPDATE_USER_INFO, exception.getErrorCode());
 
+    }
+
+    @Test
+    @DisplayName("정상 테스트 : 일반 사용자가 회원 탈퇴에 성공한다")
+    public void deleteUserSuccessTest() {
+        // given
+        Long userId = 1L;
+        String deleteReason = "오류가 잦아요";
+        DeleteReasonRequest request = new DeleteReasonRequest(deleteReason);
+
+        User user = mock(User.class);
+        DeleteReason enumReason = DeleteReason.ERROR_FREQUENT;
+
+        when(userRepository.findByUserIdAndIsDeleteFalse(userId)).thenReturn(Optional.of(user));
+
+        // when
+        userService.deleteUser(userId, request);
+
+        // then
+        verify(user).updateIsDelete();
+        verify(userDeleteService).save(user, enumReason);
+    }
+
+    @Test
+    @DisplayName("예외 테스트 : 존재하지 않는 사용자 정보로 회원 탈퇴를 시도하여 실패한다.")
+    public void deleteUserNotFoundTest() {
+        // given
+        Long userId = 1L;
+        String deleteReason = "오류가 잦아요";
+        DeleteReasonRequest request = new DeleteReasonRequest(deleteReason);
+
+        when(userRepository.findByUserIdAndIsDeleteFalse(userId)).thenReturn(Optional.empty());
+
+        // when & then
+        assertThatThrownBy(() -> userService.deleteUser(userId, request))
+            .isInstanceOf(CustomException.class)
+            .hasFieldOrPropertyWithValue("errorCode", ErrorCode.NOT_EXIST_USER);
+    }
+
+    @Test
+    @DisplayName("예외 테스트 : 유효하지 않은 탈퇴 이유 값으로 회원 탈퇴를 시도하여 실패한다.")
+    public void deleteUserWithInvalidReasonTest() {
+        // given
+        Long userId = 1L;
+
+        String invalidReason = "유효하지 않은 탈퇴 이유";
+        DeleteReasonRequest request = new DeleteReasonRequest(invalidReason);
+
+        // when & then
+        assertThatThrownBy(() -> userService.deleteUser(userId, request))
+            .isInstanceOf(CustomException.class)
+            .hasFieldOrPropertyWithValue("errorCode", ErrorCode.INVALID_DELETE_REASON);
+    }
+
+    @Test
+    @DisplayName("정상 테스트 : 소셜 로그인 사용자가 회원 탈퇴를 성공한다")
+    public void deleteSocialUserTest() {
+        // given
+        Long userId = 1L;
+        String deleteReason = "서비스 기능이 미흡해요";
+        DeleteReasonRequest request = new DeleteReasonRequest(deleteReason);
+
+        User user = mock(User.class);
+        DeleteReason enumReason = DeleteReason.POOR_FUNCTIONALITY;
+
+        when(userRepository.findByUserIdAndIsDeleteFalse(userId)).thenReturn(Optional.of(user));
+        when(user.isSocial()).thenReturn(true);
+        when(kakaoUserService.getKakaoUserByUserId(userId)).thenReturn(Optional.of(kakaoUser));
+
+        // when
+        userService.deleteUser(userId, request);
+
+        // then
+        verify(user).updateIsDelete();
+        verify(userDeleteService).save(user, enumReason);
+        verify(kakaoUserService).deleteKakaoUser(kakaoUser);
+    }
+
+    @Test
+    @DisplayName("예외 테스트 : 소셜 로그인 사용자 정보가 없어 회원 탈퇴에 실패한다")
+    public void deleteSocialUserNotFoundTest() {
+        // given
+        Long userId = 1L;
+        String deleteReason = "서비스 기능이 미흡해요";
+        DeleteReasonRequest request = new DeleteReasonRequest(deleteReason);
+
+        User user = mock(User.class);
+        when(userRepository.findByUserIdAndIsDeleteFalse(userId)).thenReturn(Optional.of(user));
+        when(user.isSocial()).thenReturn(true);
+        when(kakaoUserService.getKakaoUserByUserId(userId)).thenReturn(Optional.empty());
+
+        // when & then
+        assertThatThrownBy(() -> userService.deleteUser(userId, request))
+            .isInstanceOf(CustomException.class)
+            .hasFieldOrPropertyWithValue("errorCode", ErrorCode.KAKAO_USER_NOT_FOUND);
     }
 }
