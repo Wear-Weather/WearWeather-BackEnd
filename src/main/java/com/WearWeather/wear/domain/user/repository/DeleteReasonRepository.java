af1428cef20bdf37fd5e11f6e4446f4e3a6bfe36
이건희 <dnzp74@g.eulji.ac.kr>
2024-09-16 17:50:28 +0900

    remove : DeleteReason 도메인 관련 클래스 삭제 
     
    
     
    
     
    
     
    
     
    
     
    
     

--- src/main/java/com/WearWeather/wear/domain/user/repository/DeleteReasonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.WearWeather.wear.domain.user.repository;
-
-import com.WearWeather.wear.domain.user.entity.DeleteReason;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeleteReasonRepository extends JpaRepository<DeleteReason, Long> {
-}
