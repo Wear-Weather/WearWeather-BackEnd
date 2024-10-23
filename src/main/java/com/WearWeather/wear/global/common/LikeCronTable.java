c90011c460b01d2200106c1ae6049d6bd9fe8b4c
sky1231234 <blueskyee52@gmail.com>
2024-10-23 23:25:21 +0900

    feat: 좋아요 스케쥴링 추가

---
+++ src/main/java/com/WearWeather/wear/global/common/LikeCronTable.java
@@ -0,0 +1,60 @@
+package com.WearWeather.wear.global.common;
+
+import com.WearWeather.wear.domain.post.repository.PostRepository;
+import com.WearWeather.wear.domain.postLike.entity.Like;
+import com.WearWeather.wear.domain.postLike.repository.LikeRepository;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.Stream;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class LikeCronTable {
+
+  private final LikeRepository likeRepository;
+  private final PostRepository postRepository;
+
+  @Value("${schedule.like.use}")
+  private boolean likeSchedule;
+
+  @Scheduled(cron = "${schedule.like.cron}")
+  public void schedule() {
+
+    if (likeSchedule) {
+      System.out.println("HERE");
+      int totalLikeCount = 15;
+      List<Long> postIdsList = postRepository.find20IdsByOrderByIdDesc();
+
+      Random random = new Random();
+
+      List<Long> userIdList = Arrays.asList(11L, 17L, 21L, 63L, 64L);
+
+      List<Like> likeList = postIdsList.stream()
+          .flatMap(postId -> {
+                int randomCount = random.nextInt(3);
+
+                return randomCount == 0 ? Stream.empty() : Stream.generate(() -> {
+                      int randomIndex = random.nextInt(userIdList.size());
+                      return Like.builder()
+                          .userId(userIdList.get(randomIndex))
+                          .postId(postId)
+                          .build();
+                    })
+                    .limit(randomCount);
+              }
+          )
+          .limit(totalLikeCount)
+          .toList();
+
+//      likeRepository.saveAll(likeList);
+    }
+  }
+}
