946c7bbf7ea6963ac55a9a4beaae319643863c91
이건희 <dnzp74@g.eulji.ac.kr>
2024-10-31 22:54:16 +0900

    refactor : @LoggedInUser 파라미터 타입 Long으로 수정

--- src/main/java/com/WearWeather/wear/global/jwt/UserIdArgumentResolver.java
+++ src/main/java/com/WearWeather/wear/global/jwt/UserIdArgumentResolver.java
@@ -18,7 +18,7 @@ public class UserIdArgumentResolver implements HandlerMethodArgumentResolver {
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         return parameter.hasParameterAnnotation(LoggedInUser.class) &&
-          Optional.class.isAssignableFrom(parameter.getParameterType());
+          Long.class.isAssignableFrom(parameter.getParameterType());
     }
 
     @Override
@@ -29,14 +29,13 @@ public class UserIdArgumentResolver implements HandlerMethodArgumentResolver {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        // 인증되지 않은 경우 Optional.empty() 반환 (로그인하지 않은 사용자)
         if (authentication == null || !authentication.isAuthenticated() ||
           (authentication.getPrincipal() instanceof String && "anonymousUser".equals(authentication.getPrincipal()))) {
-            return Optional.empty();
+            return null;
         }
 
         if (authentication.getPrincipal() instanceof UserAuthentication userAuth) {
-            return Optional.of(userAuth.getPrincipal());
+            return userAuth.getPrincipal();
         }
 
         throw new IllegalArgumentException("주체가 예상되는 UserAuthentication 유형이 아닙니다");
