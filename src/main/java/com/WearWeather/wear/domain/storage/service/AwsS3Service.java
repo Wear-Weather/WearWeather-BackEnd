c3b2f6d872fd19aa90618b9f4771ff95a07e89b7
이건희 <dnzp74@g.eulji.ac.kr>
2024-11-06 23:51:18 +0900

    refactor: 이미지 형식을 webp 형식으로 변환하여 s3에 업로드 후 DB에 저장

--- src/main/java/com/WearWeather/wear/domain/storage/service/AwsS3Service.java
+++ src/main/java/com/WearWeather/wear/domain/storage/service/AwsS3Service.java
@@ -6,15 +6,20 @@ import com.WearWeather.wear.global.exception.ErrorCode;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import java.awt.image.BufferedImage;
+import com.sksamuel.scrimage.webp.WebpWriter;
+import jakarta.annotation.PostConstruct;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.UUID;
-import javax.imageio.ImageIO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.sksamuel.scrimage.ImmutableImage;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 @Service
 @RequiredArgsConstructor
@@ -23,40 +28,80 @@ public class AwsS3Service {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    private final AmazonS3 amazonS3;
+    @Value("${photo.dir}")
+    private String photoDir;
 
+    private final AmazonS3 amazonS3;
+    
     /**
-     * AWS S3에 이미지 파일 업로드
+     * 업로드할 파일을 WebP로 변환 후 S3에 저장
      *
      * @param multipartFile : 파일
-     * @param dirName       : 폴더 이름
-     * @return : FileInfoDto (path, cdn URL)
+     * @param dirName       : S3 폴더 이름
+     * @return : ImageInfoDto (path, cdn URL 등)
      */
     public ImageInfoDto upload(MultipartFile multipartFile, String dirName) {
 
-        // 이미지 파일 유효성 체크 후 bufferedImage 반환
-        BufferedImage bufferedImage = getValidBufferedImage(multipartFile);
+        // MultipartFile을 File로 변환
+        File originalFile = convertMultipartFileToFile(multipartFile);
 
-        String fileName = createFileName(multipartFile.getOriginalFilename(), dirName);
+        String baseFileName = createFileName(multipartFile.getOriginalFilename(), dirName);
+        String webpFileName = baseFileName + ".webp";
 
-        // 메타데이터 지정, TODO : 필요여부 체크
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-        objectMetadata.setContentLength(multipartFile.getSize());
-        objectMetadata.setContentType(multipartFile.getContentType());
+        // WebP로 변환
+        File webpFile = convertToWebp(webpFileName, originalFile);
 
-        // s3에 이미지 저장
-        try (InputStream inputStream = multipartFile.getInputStream()) {
-            amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata));
-        } catch (IOException e) {
-            // TODO : 추주 예외 변경
-            throw new CustomException(ErrorCode.SERVER_ERROR);
+        // S3에 업로드
+        try {
+            uploadFileToS3(webpFile, webpFileName);
+            String url = amazonS3.getUrl(bucket, webpFileName).toString();
+
+            return ImageInfoDto.of(webpFileName, url);
+        } finally {
+            originalFile.delete();
+            webpFile.delete();
         }
+    }
 
-        // s3에 저장된 파일 URL 얻어옴
-        // TODO : 이미지 조회에 대한 캐싱 적용을 위한 CDN 적용 필요
-        String url = amazonS3.getUrl(bucket, fileName).toString();
+    private File convertToWebp(String fileName, File originalFile) {
+        try {
+            return ImmutableImage.loader()
+              .fromFile(originalFile)
+              .output(WebpWriter.DEFAULT, new File(photoDir, fileName + ".webp"));
+        } catch (IllegalArgumentException e) {
+            throw new CustomException(ErrorCode.INVALID_IMAGE_FORMAT);
+        } catch (IOException e) {
+            throw new CustomException(ErrorCode.WEBP_CONVERSION_FAILED);
+        }
+    }
 
-        return ImageInfoDto.of(fileName, url, bufferedImage.getWidth(), bufferedImage.getHeight());
+    private void uploadFileToS3(File file, String fileName) {
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(file.length());
+            metadata.setContentType("image/webp");
+
+            amazonS3.putObject(new PutObjectRequest(bucket, fileName, fileInputStream, metadata));
+        } catch (IOException e) {
+            throw new CustomException(ErrorCode.S3_UPLOAD_FAILED);
+        }
+    }
+
+    private File convertMultipartFileToFile(MultipartFile file) {
+        try {
+            File convFile = new File(file.getOriginalFilename());
+            try (FileOutputStream fos = new FileOutputStream(convFile)) {
+                fos.write(file.getBytes());
+            }
+            return convFile;
+        } catch (IOException e) {
+            throw new CustomException(ErrorCode.NOT_FOUND_IMAGE_FILE);
+        }
+    }
+
+    private String createFileName(String fileName, String dirName) {
+        String baseName = fileName.contains(".") ? fileName.substring(0, fileName.lastIndexOf(".")) : fileName;
+        return dirName + "/" + UUID.randomUUID() + "_" + baseName;
     }
 
     public void delete(String fileName) {
@@ -66,39 +111,4 @@ public class AwsS3Service {
     public String getUrl(String fileName) {
         return amazonS3.getUrl(bucket, fileName).toString();
     }
-
-    /**
-     * 이미지 유효성 체크 후 BufferedImage 반환
-     */
-    public BufferedImage getValidBufferedImage(MultipartFile multipartFile) {
-        try {
-            // 업로드된 파일의 BufferedImage 를 가져온다.
-            BufferedImage read = ImageIO.read(multipartFile.getInputStream());
-
-            // 업로드된 파일이 이미지가 아닐 경우를 체크한다.
-            if (read == null) {
-                throw new CustomException(ErrorCode.IMAGE_NOT_FOUND);
-            }
-
-            // 파일 확장자 체크 [jpeg, jpg, png, gif]
-            if (!isValidImageExtension(multipartFile.getContentType())) {
-                throw new CustomException(ErrorCode.IMAGE_INVALID_FILE_FORMAT);
-            }
-
-            return read;
-        } catch (IOException e) {
-            throw new CustomException(ErrorCode.SERVER_ERROR);
-        }
-    }
-
-    private String createFileName(String fileName, String dirName) {
-        // 파일명을 다르게 하기 위해 UUID 를 붙임
-        return dirName + "/" + UUID.randomUUID() + fileName;
-    }
-
-    private boolean isValidImageExtension(String contentType) {
-        // 유효한 이미지 확장자.
-        return contentType.matches("^image/(jpeg|jpg|png|gif)$");
-    }
-
 }
