602182676618e1645ce86fd154ccaf21076f7b2d
이건희 <dnzp74@g.eulji.ac.kr>
2024-12-18 15:03:21 +0900

    refactor(AuthController): 각 기능에 맞는 facade 클래스를 의존하도록 수정

--- src/main/java/com/WearWeather/wear/domain/auth/controller/AuthController.java
+++ src/main/java/com/WearWeather/wear/domain/auth/controller/AuthController.java
@@ -5,7 +5,9 @@ import static com.WearWeather.wear.global.jwt.JwtFilter.AUTHORIZATION_HEADER;
 import com.WearWeather.wear.domain.auth.dto.request.LoginRequest;
 import com.WearWeather.wear.domain.auth.dto.response.LoginResponse;
 import com.WearWeather.wear.domain.auth.dto.response.TokenResponse;
-import com.WearWeather.wear.domain.auth.service.AuthService;
+import com.WearWeather.wear.domain.auth.facade.LogOutFacade;
+import com.WearWeather.wear.domain.auth.facade.LoginFacade;
+import com.WearWeather.wear.domain.auth.facade.ReissueFacade;
 import com.WearWeather.wear.global.common.dto.ResponseCommonDTO;
 import com.WearWeather.wear.global.exception.CustomException;
 import com.WearWeather.wear.global.exception.ErrorCode;
@@ -16,10 +18,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import java.util.Arrays;
-import java.util.Optional;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -30,28 +30,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final AuthService authService;
+    private final LoginFacade loginFacade;
+    private final LogOutFacade logOutFacade;
+    private final ReissueFacade reissueFacade;
     private final TokenProvider tokenProvider;
 
-    public AuthController(AuthService authService, TokenProvider tokenProvider) {
-        this.authService = authService;
+    public AuthController(TokenProvider tokenProvider, LoginFacade loginFacade, LogOutFacade logOutFacade, ReissueFacade reissueFacade) {
         this.tokenProvider = tokenProvider;
+        this.loginFacade = loginFacade;
+        this.logOutFacade = logOutFacade;
+        this.reissueFacade = reissueFacade;
     }
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request, HttpServletResponse response) {
-        LoginResponse loginResponse = authService.checkLogin(request);
+        LoginResponse loginResponse = loginFacade.checkLogin(request);
 
         String refreshToken = tokenProvider.renewRefreshToken(loginResponse.getAccessToken());
         ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken", refreshToken)
-            .path("/")
-            .sameSite("Strict")
-            .httpOnly(true)
-            .secure(true)
-            .domain("lookattheweather.store")
-            .maxAge(7 * 24 * 60 * 60)
-            .build();
-
+          .path("/")
+          .sameSite("Strict")
+          .httpOnly(true)
+          .secure(true)
+          .domain("lookattheweather.store")
+          .maxAge(7 * 24 * 60 * 60)
+          .build();
         response.addHeader("Set-Cookie", refreshTokenCookie.toString());
 
         return ResponseEntity.ok(loginResponse);
@@ -60,7 +63,7 @@ public class AuthController {
     @PostMapping("/logout")
     public ResponseEntity<ResponseCommonDTO> logout(@LoggedInUser Long userId, @RequestHeader(AUTHORIZATION_HEADER) String tokenHeader, HttpServletResponse response) {
         String token = tokenHeader.replace("Bearer ", "");
-        authService.logout(userId, token);
+        logOutFacade.logout(userId, token);
 
         ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken", "")
           .path("/")
@@ -70,7 +73,6 @@ public class AuthController {
           .domain("lookattheweather.store")
           .maxAge(0)
           .build();
-
         response.addHeader("Set-Cookie", refreshTokenCookie.toString());
 
         return ResponseEntity.ok(new ResponseCommonDTO(true, "success logout"));
@@ -84,16 +86,16 @@ public class AuthController {
         }
 
         String refreshToken = Arrays.stream(cookies)
-            .filter(cookie -> "refreshToken".equals(cookie.getName()))
-            .map(Cookie::getValue)
-            .findFirst()
-            .orElse(null);
+          .filter(cookie -> "refreshToken".equals(cookie.getName()))
+          .map(Cookie::getValue)
+          .findFirst()
+          .orElse(null);
 
         if (refreshToken == null || refreshToken.isEmpty()) {
             throw new CustomException(ErrorCode.NOT_FOUND_REFRESH_TOKEN_IN_COOKIE);
         }
 
-        TokenResponse newToken = authService.reissue(refreshToken);
+        TokenResponse newToken = reissueFacade.reissue(refreshToken);
         return ResponseEntity.ok(newToken);
     }
 }
