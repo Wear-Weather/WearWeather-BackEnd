f86868c250f893fba97528e840b1aacb6b234d6d
이건희 <dnzp74@g.eulji.ac.kr>
2024-12-19 14:15:16 +0900

    refactor(KaKaoUserService): 소셜 로그인 해제 로직 관련 서비스에서 책임하도록 수정

--- src/main/java/com/WearWeather/wear/domain/oauth/infrastructure/kakao/service/KakaoUserService.java
+++ src/main/java/com/WearWeather/wear/domain/oauth/infrastructure/kakao/service/KakaoUserService.java
@@ -1,9 +1,13 @@
 package com.WearWeather.wear.domain.oauth.infrastructure.kakao.service;
 
 import com.WearWeather.wear.domain.oauth.infrastructure.kakao.KaKaoUserInfo;
+import com.WearWeather.wear.domain.oauth.infrastructure.kakao.dto.KakaoUserDto;
 import com.WearWeather.wear.domain.oauth.infrastructure.kakao.entity.KakaoUser;
 import com.WearWeather.wear.domain.oauth.infrastructure.kakao.repository.KakaoUserRepository;
+import com.WearWeather.wear.domain.oauth.service.RequestOAuthUnlinkService;
 import com.WearWeather.wear.domain.user.entity.User;
+import com.WearWeather.wear.global.exception.CustomException;
+import com.WearWeather.wear.global.exception.ErrorCode;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class KakaoUserService {
 
+    private final RequestOAuthUnlinkService requestOAuthUnlinkService;
     private final KakaoUserRepository kakaoUserRepository;
 
     @Transactional
@@ -36,4 +41,11 @@ public class KakaoUserService {
         kakaoUserRepository.delete(kakaoUser);
     }
 
+    public void unlinkOauth(Long userId) {
+        KakaoUser kakaoUser = getKakaoUserByUserId(userId)
+          .orElseThrow(() -> new CustomException(ErrorCode.KAKAO_USER_NOT_FOUND));
+
+        requestOAuthUnlinkService.request(KakaoUserDto.of(kakaoUser));
+        deleteKakaoUser(kakaoUser);
+    }
 }
