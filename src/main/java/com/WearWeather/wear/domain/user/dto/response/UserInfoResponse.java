ebee92719973c16c12d7929a9a8b697898cadf06
이건희 <dnzp74@g.eulji.ac.kr>
2024-12-19 11:48:47 +0900

    refactor(UserInfoResponse): isSocial 필드 추가하여 반환

--- src/main/java/com/WearWeather/wear/domain/user/dto/response/UserInfoResponse.java
+++ src/main/java/com/WearWeather/wear/domain/user/dto/response/UserInfoResponse.java
@@ -9,26 +9,31 @@ import lombok.Getter;
 @Builder
 @Getter
 public class UserInfoResponse {
+
     private final String email;
     private final String name;
     private final String nickname;
+    private final boolean isSocial;
 
     @JsonCreator
     public UserInfoResponse(
-            @JsonProperty("email") String email,
-            @JsonProperty("name") String name,
-            @JsonProperty("nickname") String nickname) {
+      @JsonProperty("email") String email,
+      @JsonProperty("name") String name,
+      @JsonProperty("nickname") String nickname,
+      @JsonProperty("isSocial") boolean isSocial) {
         this.email = email;
         this.name = name;
         this.nickname = nickname;
+        this.isSocial = isSocial;
     }
 
     public static UserInfoResponse of(User user) {
         return UserInfoResponse.builder()
-                .email(user.getEmail())
-                .name(user.getName())
-                .nickname(user.getNickname())
-                .build();
+          .email(user.getEmail())
+          .name(user.getName())
+          .nickname(user.getNickname())
+          .isSocial(user.isSocial())
+          .build();
     }
 
 }
